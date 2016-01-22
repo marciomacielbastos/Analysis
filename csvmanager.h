defbaa066d240f785822831f1521c73ff2124de0
marciomacielbastos <marciomacielbastos@hotmail.com>
2016-01-22 00:26:01 -0500

    usage therms added

--- csvmanager.h
+++ csvmanager.h
@@ -1,3 +1,21 @@
+/*************************************************************************
+ *
+ * Author: Marcio Maciel Bastos
+ * __________________
+ *
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Marcio Maciel Bastos.
+ * The intellectual and technical concepts contained
+ * herein are proprietary to Marcio Maciel Bastos
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Marcio Maciel Bastos.
+ */
+
 #ifndef CSVMANAGER_H
 #define CSVMANAGER_H
 #include <queue>
